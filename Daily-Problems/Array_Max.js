0e81dd1c3490e62e7d99fe35e5f02c4a23997b96
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-07 12:49:08 -0500

    Array max solution

---
+++ Daily-Problems/Array_Max.js
@@ -0,0 +1,12 @@
+// completed 9-6-2016
+
+// Array Max
+// Given an array of negative/positive integers, find the largest element. 
+// DO NOT use Math.max
+
+// Solution //
+
+function max (numbers) {
+ 	// Reduce the array of numbers returning the greater of the two each iteration.
+	return numbers.reduce((a,b) => a < b ? b : a)w
+};
