4db5995097983e4f6a6720be619a4fa60ade173e
Dillon Lundell <dillonjaylundell@gmail.com>
2021-03-24 21:31:34 -0700

    test commit

--- Daily-Problems/Alphabet_Inventory.js
+++ Daily-Problems/Alphabet_Inventory.js
@@ -6,25 +6,32 @@
 // Input	                                       Output
 // alphabet:"aBc" text:"aabbccdd"               |  "a:2,b:2,c:2"
 // alphabet:"x" text:"Racer X is my friend :)"	|  "x:1"
-// alphabet:"123" text: "o hai"	                |  "no matches" 
+// alphabet:"123" text: "o hai"	                |  "no matches"
 
-// Solution // 
+// Solution //
 
-function alphaCount (alphabet, text) {
-	// Create an array to store the answer.
-	var answer = [];
-	// First make the entire text argument lowercase.
-	// Create an object with the character count of the text argument. Ex: {a:2, b:3, c:5}.
-	var chCnt = text.toLowerCase().split('').reduce((a,b) => {
-    	a[b] ? a[b]++ : a[b] = 1;
-    	return a
-	},{});
-	// Make enitre alphabet argument lowercase.
-	// Make alphabet an array and loop through it. 
-	alphabet.toLowerCase().split('').forEach(item => {
-		// If the chCnt object has the item as a property push push the character into the answer array. Ex: a:4.
-    	chCnt.hasOwnProperty(item) ? answer.push(item + ':' + chCnt[item]) : null;
-	});
-	// If the answer array is empty return 'no matches'. If not return the answer array joined to a string.
-	return answer.length === 0 ? 'no matches' : answer.join();
+function alphaCount(alphabet, text) {
+  // Create an array to store the answer.
+  var answer = [];
+  // First make the entire text argument lowercase.
+  // Create an object with the character count of the text argument. Ex: {a:2, b:3, c:5}.
+  var chCnt = text
+    .toLowerCase()
+    .split("")
+    .reduce((a, b) => {
+      a[b] ? a[b]++ : (a[b] = 1);
+      return a;
+    }, {});
+  // This is a test commit.
+  // Make enitre alphabet argument lowercase.
+  // Make alphabet an array and loop through it.
+  alphabet
+    .toLowerCase()
+    .split("")
+    .forEach((item) => {
+      // If the chCnt object has the item as a property push push the character into the answer array. Ex: a:4.
+      chCnt.hasOwnProperty(item) ? answer.push(item + ":" + chCnt[item]) : null;
+    });
+  // If the answer array is empty return 'no matches'. If not return the answer array joined to a string.
+  return answer.length === 0 ? "no matches" : answer.join();
 }
